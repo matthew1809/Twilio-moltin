78c7c6763de940348c00fca5dfe9ae67a9a74530
Matthew Foyle <matt@moltin.com>
2017-10-11 14:35:54 +0100

    amended success url

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ var port = process.env.PORT || 5000;
 
 //open the app on port 5000
 app.listen(port, function () {
-  console.log('Twilio - Moltin app listening on port' + port);
+  console.log('Twilio - Moltin app listening on port ' + port);
 });
 
 
