c41c214e6d946fbe2162f4165995f3fbdd678a90
Matthew Foyle <matt@moltin.com>
2017-10-13 10:34:21 +0100

    stringify req body

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post('/orders', jsonParser, function (req, res) {
 
 app.post('/sms', (req, res) => {
  
-  console.log(req);
+  console.log((JSON.stringify(req.body));
 
   // const twiml = new MessagingResponse();
 
