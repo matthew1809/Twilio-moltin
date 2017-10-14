ce19b91dfe6f0578bc19092ee51d3e4c3afe79ae
Matthew Foyle <matt@moltin.com>
2017-10-14 17:06:38 +0100

    exports function

--- twilio.js
+++ twilio.js
@@ -12,7 +12,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 var exports = module.exports = {};
 
 // create a twilio sms
-var createMessage = function(to, name, total, id) {
+exports.createMessage = function(to, name, total, id) {
 
 	client.messages.create(
 		{ 
