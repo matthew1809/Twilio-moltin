280a526ed4473eb154e5e515c689bcefb4bcaa30
Matthew Foyle <matt@moltin.com>
2018-02-05 12:18:47 +0000

    - Improve running order

--- moltin.js
+++ moltin.js
@@ -6,13 +6,22 @@ var exports = module.exports = {};
 //require and initialise the moltin SDK
 const MoltinGateway = require('@moltin/sdk').gateway;
 const Moltin = MoltinGateway({
-  client_id: process.env.client_id,
-  client_secret: process.env.client_secret,
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
 });
 
 // function to get a moltin order given an order ID
 exports.getOrder = function(ID) {
 	return Moltin.Orders.Get(ID);
+};
+
+exports.getOrdersByEmail = function(address) {
+
+	return Moltin.Orders.Filter({
+    	eq: {
+        	email: address
+        }
+	}).All()
 };
 
 // function to get a moltin customer given an customer ID
